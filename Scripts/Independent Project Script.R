b0c913f0cf3a9c6c152a2238c6df21668f7a2827
lexiefoster <142915950+lexiefoster@users.noreply.github.com>
2023-11-02 13:44:19 -0500

    11/2/23 work session

--- Scripts/Independent Project Script.R
+++ Scripts/Independent Project Script.R
@@ -13,10 +13,31 @@ view(Plant_Exudate_Datasheet)
 
 # Turning dates-as-characters into "date" objects
 dat %>%
-  mutate(planting_date=as.Date(dat$Planting.date, format="%m/%d/%y"))
+  mutate(Planting date=as.Date(dat$Planting date, format="%m/%d/%y")) %>%
+  mutate(root weight taken date=as.Date(dat$`Root Weight Taken Date`, format="%m/%d/%y")) %>%
+  mutate(Date AA/OA Data Received=as.Date(dat$`Date AA/OA Data Received`, format="%m%d%y"))
 
-as.Date(Plant_Exudate_Datasheet$`Root Weight Taken Date`, format="%m/%d/%y")
-as.Date(Plant_Exudate_Datasheet$`Date AA/OA Data Received`, format="%m%d%y")
+## 11/1/23 - work session - trying to visualize different plots using my dataset
+colorset=rainbow(200)
+pt.cols=colorset[as.numeric(dat$`Genotype ID`)]
+plot(Trp~Root weight (g), data=dat, xlab="Root weight (g)", ylab="Trp", las=1, pch=19, col=pt.cols)
+
+#typical boxplot
+ggplot(dat, aes(x="Genotype ID", y="Trp", fill="Genotype ID")) +
+  geom_boxplot(notch=T) +
+  scale_fill_brewer(palette="RdYlBu") +
+  ylab("Sepal Width")
+
+# step 1: define the data and aesthetics
+library(ggplot2)
+ggplot(data=dat, mapping=aes(x="Genotype ID", y="Trp")) # this is not working
+
+#step 2: add scatter plot using geom_point()
+ggplot(dat, aes(x="Root weight (g)", y="Trp", color="Genotype ID")) + geom_point(size=2) # this is not working either
+
+## For next time: figure out why my graph codes are not displaying the data correctly
+
+
 
 
 
