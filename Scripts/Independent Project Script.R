073f8a4a0648d9e4dfef4a1eca34c97f3582256e
dshizuka <dshizuka2@unl.edu>
2023-11-07 13:05:02 -0600

    Update Independent Project Script.R

--- Scripts/Independent Project Script.R
+++ Scripts/Independent Project Script.R
@@ -13,14 +13,19 @@ view(Plant_Exudate_Datasheet)
 
 # Turning dates-as-characters into "date" objects
 dat %>%
-  mutate(Planting date=as.Date(dat$Planting date, format="%m/%d/%y")) %>%
-  mutate(root weight taken date=as.Date(dat$`Root Weight Taken Date`, format="%m/%d/%y")) %>%
-  mutate(Date AA/OA Data Received=as.Date(dat$`Date AA/OA Data Received`, format="%m%d%y"))
+  mutate(Planting.date=as.Date(dat$Planting.date, format="%m/%d/%y")) %>%
+  mutate(root.weight.taken.date=as.Date(dat$`Root Weight Taken Date`, format="%m/%d/%y")) %>%
+  mutate(Date.AA.OA.Data.Received=as.Date(dat$`Date AA/OA Data Received`, format="%m%d%y"))
 
 ## 11/1/23 - work session - trying to visualize different plots using my dataset
 colorset=rainbow(200)
-pt.cols=colorset[as.numeric(dat$`Genotype ID`)]
-plot(Trp~Root weight (g), data=dat, xlab="Root weight (g)", ylab="Trp", las=1, pch=19, col=pt.cols)
+pt.cols=colorset[as.numeric(dat$Genotype.ID)]
+plot(Trp~Root.weight..g., data=dat, xlab="Root weight (g)", ylab="Trp", las=1, pch=19)
+
+##ggplot way
+ggplot(dat, aes(x=Root.weight..g., y=Trp, color=Genotype.ID)) +
+  geom_point() +
+  theme(legend.position = "none")
 
 #typical boxplot
 ggplot(dat, aes(x="Genotype ID", y="Trp", fill="Genotype ID")) +
